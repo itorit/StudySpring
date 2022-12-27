2bab5352a35fe5a489cc13532d96ec9d700ac5d0
JUN <bsjzzang3@gmail.com>
2022-12-28 07:57:01 +0900

    의존관계 자동 주입 - @Autowired 필드 명, @Qualifier, @Primary

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -2,7 +2,12 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
 
+@Component
+//@Qualifier("fixDiscountPolicy")
+@Primary //자식이 여러개일 경우 우선권을 가진다.
 public class FixDiscountPolicy implements DiscountPolicy {
 
     private int discountFixAmount = 1000; //1000원 할인
