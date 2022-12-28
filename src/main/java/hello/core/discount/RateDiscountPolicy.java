17f915c38ad3a65c122194acb3fc12f86a5c951c
JUN <bsjzzang3@gmail.com>
2022-12-28 09:05:30 +0900

    의존관계 자동 주입 - 조회한 빈이 모두 필요할때, List, Map

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -1,11 +1,13 @@
 package hello.core.discount;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import org.springframework.stereotype.Component;
 
 @Component
 //@Qualifier("mainDiscountPolicy")
+@MainDiscountPolicy //어노테이션을 정의 해서 사용하는 방법은 다른사람이 유지보수 할때 햇갈리므로 남발하지말자
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountPercent = 10;
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,13 +1,15 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
@@ -16,10 +18,11 @@ public class OrderServiceImpl implements OrderService {
     // Lombok 어노테이션 @RequiredArgsConstructor이
     // 필드에 final 이 붙은 객체의 생성자를 만들어준다.
     // 그래서 @RequiredArgsConstructor 사용했으면 아래 코드는 필요없다.
-//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-//        this.memberRepository = memberRepository;
-//        this.discountPolicy = discountPolicy;
-//    }
+    @Autowired //생성자가 한개라면 @Autowired 붙일 필요없다.
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
