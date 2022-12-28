cac012526942f6969ef6ed7021479ff44f7a2333
JUN <bsjzzang3@gmail.com>
2022-12-28 11:40:42 +0900

    빈 생명주기 콜백 - 빈 등록 초기화, 소멸 메서드

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,9 +1,6 @@
 package hello.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 
     private String url;
 
@@ -28,7 +25,7 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close: " + url);
     }
 
-    @Override
+    /*@Override
     public void afterPropertiesSet() throws Exception {
         System.out.println("NetworkClient.afterPropertiesSet");
         connect();
@@ -38,6 +35,17 @@ public class NetworkClient implements InitializingBean, DisposableBean {
     @Override
     public void destroy() throws Exception {
         System.out.println("NetworkClient.destroy");
+        disconnect();
+    }*/
+
+    public void init() {
+        System.out.println("NetworkClient.init");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    public void close() {
+        System.out.println("NetworkClient.close");
         disconnect();
     }
 }
