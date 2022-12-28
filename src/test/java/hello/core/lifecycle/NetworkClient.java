c790e83e573fcc2302868a22033782fc62f21ea3
JUN <bsjzzang3@gmail.com>
2022-12-28 12:01:55 +0900

    빈 생명주기 콜백 - 애노테이션 @PostConstruct, @PreDestroy

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,5 +1,8 @@
 package hello.core.lifecycle;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient {
 
     private String url;
@@ -25,7 +28,9 @@ public class NetworkClient {
         System.out.println("close: " + url);
     }
 
-    /*@Override
+    /*
+     * 스프링에서의 권고는 @PostConstruct, @PreDestroy 사용
+     * @Override
     public void afterPropertiesSet() throws Exception {
         System.out.println("NetworkClient.afterPropertiesSet");
         connect();
@@ -38,12 +43,14 @@ public class NetworkClient {
         disconnect();
     }*/
 
+    @PostConstruct
     public void init() {
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
+    @PreDestroy
     public void close() {
         System.out.println("NetworkClient.close");
         disconnect();
